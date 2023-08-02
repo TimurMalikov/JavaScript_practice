8f585ed3171cc0822b0fb912f91c74c2dad47e44
Timur <timurmalikov0305@gmail.com>
2023-08-02 22:38:57 +0300

    Homework_#1

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+let num = 26;
+let Num = 4;
+let str = 'congratulation';
+let bln = true;
+
+console.log(`${num} - ${typeof(num)}`);
+console.log(Num, typeof(Num));
+console.log(`${str} - ${typeof(str)}`);
+console.log(bln, typeof(bln));
+
+console.log(num + Num);
+console.log(num - Num);
+console.log(num * Num);
+console.log(num / Num);
+
+let value = [];
+
+value.push('one', 'two', 'three');
+console.log(value);
+
+// let variable1 = 0n;
+// console.log(typeof variable1);
+//
+// let nan1 = NaN;
+// console.log(typeof nan1);
+//
+//
+// console.log(3 + 3);
+// console.log('3' + '3');
+// console.log('3' + + 3);
+// console.log(3 + + '3');
+// console.log(3 + 3 - 3);
+// console.log('3' + '3' - '3');
