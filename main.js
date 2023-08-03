7e45b7a088b5e085acacd7dc687847b293263f7f
Timur <timurmalikov0305@gmail.com>
2023-08-03 18:28:17 +0300

    HomeWork_#1

--- main.js
+++ main.js
@@ -19,21 +19,23 @@ console.log(num / Num);
 
 // Exercise 2*
 
-let value = [];
+let element = 'Вода Земля Огонь Воздух';
+let elementArr = element.split(' ');
+console.log(elementArr);
 
-value.push('one', 'two', 'three');
-console.log(value);
+/*let variable1 = 0n;
+console.log(typeof variable1);
 
-// let variable1 = 0n;
-// console.log(typeof variable1);
-//
-// let nan1 = NaN;
-// console.log(typeof nan1);
-//
-//
-// console.log(3 + 3);
-// console.log('3' + '3');
-// console.log('3' + + 3);
-// console.log(3 + + '3');
-// console.log(3 + 3 - 3);
-// console.log('3' + '3' - '3');
+let nan1 = NaN;
+console.log(typeof nan1);
+
+
+console.log(3 + 3);
+console.log('3' + '3');
+console.log('3' + + 3);
+console.log(3 + + '3');
+console.log(3 + 3 - 3);
+console.log('3' + '3' - '3');*/
+
+
+
